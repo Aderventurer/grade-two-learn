d729f203e1c9292b6525f3ef413603ea4c6f0780
Adventurer <2055976331@qq.com>
2024-09-25 16:26:20 +0800

    generate .txt file

--- C/random_number.c
+++ C/random_number.c
@@ -4,10 +4,68 @@
 
 #define MAX_NUMBERS 100
 
-// 比较函数，用于 qsort
 int compare(const void *a, const void *b)
 {
     return (*(double *)a > *(double *)b) - (*(double *)a < *(double *)b);
+}
+
+void generate_random_numbers(int n)
+{
+    FILE *random_file = fopen("random.txt", "w");
+    if (!random_file)
+    {
+        perror("无法打开文件 random.txt");
+        exit(1);
+    }
+
+    srand(time(NULL));
+    for (int i = 0; i < n; i++)
+    {
+        double num = (double)rand() / RAND_MAX;
+
+        fprintf(random_file, "%f\n", num);
+    }
+    fclose(random_file);
+}
+
+double *read_numbers_from_file(int *size)
+{
+    FILE *input_file = fopen("random.txt", "r");
+    if (!input_file)
+    {
+        perror("无法打开文件 random.txt");
+        return NULL;
+    }
+
+    double *numbers = malloc(MAX_NUMBERS * sizeof(double));
+    *size = 0;
+
+    while (fscanf(input_file, "%lf", &numbers[*size]) == 1)
+    {
+        (*size)++;
+        if (*size >= MAX_NUMBERS)
+            break;
+    }
+
+    fclose(input_file);
+    return numbers;
+}
+
+void write_numbers_to_file(const char *filename, double *numbers, int size)
+{
+    FILE *output_file = fopen(filename, "w");
+    if (!output_file)
+    {
+        perror("无法打开文件 sorted.txt");
+        return;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        fprintf(output_file, "%f\n", numbers[i]);
+    }
+
+    fclose(output_file);
 }
 
 int main(int argc, char *argv[])
@@ -20,38 +78,26 @@ int main(int argc, char *argv[])
 
     int n = atoi(argv[1]);
 
-    // 检查 n 的有效性
     if (n < 1 || n > MAX_NUMBERS)
     {
         fprintf(stderr, "Error: n must be a positive integer between 1 and 100.\n");
         return 1;
     }
 
-    // 生成 n 个随机数
-    double numbers[MAX_NUMBERS];
-    srand(time(NULL)); // 设置随机种子
+    generate_random_numbers(n);
 
-    for (int i = 0; i < n; i++)
+    int size;
+    double *numbers = read_numbers_from_file(&size);
+    if (numbers == NULL)
     {
-        numbers[i] = (double)rand() / RAND_MAX; // 生成 0 到 1 之间的随机数
+        return 1;
     }
 
-    // 打印随机数
-    printf("Generated random numbers:\n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%f\n", numbers[i]);
-    }
+    qsort(numbers, size, sizeof(double), compare);
 
-    // 排序
-    qsort(numbers, n, sizeof(double), compare);
+    write_numbers_to_file("sorted.txt", numbers, size);
 
-    // 打印排序后的随机数
-    printf("Sorted random numbers:\n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%f\n", numbers[i]);
-    }
+    free(numbers);
 
     return 0;
 }
